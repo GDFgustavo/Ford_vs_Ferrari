c219f122426208ea7087a5d6591ba41d62788b16
Gustavo GDF <gustavoaguiar398@gmail.com>
2024-05-13 11:04:24 -0300

    adicionando a seção de detalhes

---
+++ src/main.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('[data-tab-button]');
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function(botao) {
+            const abaAlvo = botao.target.dataset.tabButton;
+            const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
+            aba.classList.toggle('details__list--is-active');
+            botao.target.classList.toggle('details__tabs__button--is-active');
+        })
+    }
+})
