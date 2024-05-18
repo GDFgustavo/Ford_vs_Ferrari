68b30d74cef230288952c745cca7be1400fa7a6a
Gustavo GDF <gustavoaguiar398@gmail.com>
2024-05-18 09:37:30 -0300

    criando o header

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
 
+    const heroSection = document.querySelector('.header');
+    const heigthHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY;
+
+        if(posicaoAtual > heigthHero) {
+            ocultaBackgroundDoHeader();
+        } else {
+            exibeBackgroundDoHeader();
+        }
+    })
+
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
             const abaAlvo = botao.target.dataset.tabButton;
@@ -10,3 +23,13 @@ document.addEventListener('DOMContentLoaded', function() {
         })
     }
 })
+
+function ocultaBackgroundDoHeader() {
+    const header = document.querySelector('.header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeBackgroundDoHeader() {
+    const header = document.querySelector('.header');
+    header.classList.remove('header--is-hidden');
+}
